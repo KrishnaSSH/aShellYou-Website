fd601ba2e231393f28d44949f69ae21a15dbfe24
krishna <Krishna.pytech@gmail.com>
2024-08-18 17:45:54 +0530

    added footer

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Analytics } from "@vercel/analytics/react"
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Features from './components/Features';
+import Footer from "./components/Footer";
 // import Team from './components/Team';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Home/>
       <Features/>
       <Analytics />
+      <Footer/>
       {/* <Team/> */}
     </div>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './css/Footer.css'; // Import the custom CSS
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <a href="https://github.com/dp-hridayan/ashellyou/" target="_blank" rel="noopener noreferrer" className="footer-link">
+          GitHub
+        </a>
+        <a href="https://buymeacoffee.com/hridayan" className="footer-link">
+          Support Us
+        </a>
+        <a href="https://github.com/DP-Hridayan/aShellYou/graphs/contributors" className="footer-link">
+          Team
+        </a>
+        <a href="home" className="footer-link">
+          Home
+        </a>
+
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
