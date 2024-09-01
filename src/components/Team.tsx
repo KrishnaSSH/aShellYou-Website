e85be9bacfbe422e0d7c47aea96cfb16fe8681d5
krishna <Krishna.pytech@gmail.com>
2024-09-01 20:24:28 +0530

    added dynamic pfps

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -3,11 +3,10 @@ import './css/Team.css';
 
 const Team = () => {
   const teamMembers = [
-    { name: 'Krishna', job: 'Developer UI/UX Designer Website Developer', github: 'https://github.com/krishnassh' },
-    { name: 'Hridayan', job: 'Developer/Functionality Dev', github: 'https://github.com/dp-hridayan' },
-    { name: 'Starry Shivam', job: 'Bug Hunter/Developer', github: 'https://github.com/starry-shivam' },
-    { name: 'WeiguangTWK', job: 'App Tester/Chinese Translator ', github: 'https://github.com/WeiguangTWK' },
-   
+    { name: 'Krishna', job: 'Developer UI/UX Designer Website Developer', github: 'https://github.com/krishnassh', avatar: 'https://avatars.githubusercontent.com/u/125943937?v=4' },
+    { name: 'Hridayan', job: 'Developer/Functionality Dev', github: 'https://github.com/dp-hridayan', avatar: 'https://avatars.githubusercontent.com/u/157479796?v=4' }, 
+    { name: 'Starry Shivam', job: 'Bug Hunter/Developer', github: 'https://github.com/starry-shivam', avatar: 'https://avatars.githubusercontent.com/u/58552395?v=4' },
+    { name: 'WeiguangTWK', job: 'App Tester/Chinese Translator', github: 'https://github.com/WeiguangTWK', avatar: 'https://avatars.githubusercontent.com/u/41853928?v=4' },
   ];
 
   return (
@@ -16,7 +15,9 @@ const Team = () => {
       <div className="team-container">
         {teamMembers.map((member, index) => (
           <a href={member.github} key={index} className="team-member" target="_blank" rel="noopener noreferrer">
-            <div className="avatar">{member.name.charAt(0)}</div>
+            <div className="avatar">
+              <img src={member.avatar} alt={`${member.name}'s avatar`} className="avatar-img" />
+            </div>
             <h3 className="name">{member.name}</h3>
             <p className="job">{member.job}</p>
           </a>
